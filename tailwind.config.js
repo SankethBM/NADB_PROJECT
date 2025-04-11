33dbe9a1ee62757f4e138bb2a3947e9888e78431
Sanketh B M <sankethbm333@gmail.com>
2025-04-11 09:23:20 +0530

    Done

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+// tailwind.config.js
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        'primary': '#FF4242', // Reddish-orange
+        'secondary': '#2196F3', // Blue
+        'accent': '#E91E63', // Pink
+      }
+    }
+  }
+}
